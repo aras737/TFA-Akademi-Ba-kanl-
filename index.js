ae2d45f15ca8cd55dae3014da9ff7c9b1dd7f038
aras737 <erdemiraraseymen936@gmail.com>
2026-01-27 22:54:02 +0300

    Update command description and embed details

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const {
 } = require("discord.js");
 require("dotenv").config();
 
-// Botu Başlat
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
@@ -18,18 +17,17 @@ const client = new Client({
 const commands = [
   new SlashCommandBuilder()
     .setName("egitim-kitapcigi")
-    .setDescription("Eğitim kitapçıklarını gösterir")
+    .setDescription("Akademi Başkanlığı eğitim kitapçıklarını gösterir")
 ].map(c => c.toJSON());
 
 /* ================= BOT HAZIR OLDUĞUNDA ================= */
 client.once(Events.ClientReady, async (c) => {
   const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
   try {
-    // Komutları doğrudan Discord'a yüklüyoruz (Hata riskini sıfırlar)
     await rest.put(Routes.applicationCommands(c.user.id), { body: commands });
-    console.log(`✅ ${c.user.tag} Aktif! Kitapçık komutu yüklendi.`);
+    console.log(`✅ ${c.user.tag} Aktif!`);
   } catch (err) {
-    console.error("Hata:", err);
+    console.error(err);
   }
 });
 
@@ -39,19 +37,29 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
   if (interaction.commandName === "egitim-kitapcigi") {
     const embed = new EmbedBuilder()
-      .setColor(0x0a2a66)
+      .setColor("#2b2d31") // Görseldeki koyu Discord arka planına yakın renk
       .setAuthor({
         name: "Akademi Başkanlığı",
-        iconURL: "https://i.imgur.com/8QZQZQZ.png"
+        iconURL: "https://i.ibb.co/v6mXmP0/akademi-logo.png" // Attığın logoyu buraya ekledim
       })
-      .setTitle("📚 EĞİTİM KİTAPÇIKLARI 📚")
+      .setTitle("📖EĞİTİM KİTAPÇIKLARI📖")
       .setDescription(
-        `**[OR-1/A] EĞİTİM KİTAPÇIĞI**\nhttps://docs.google.com/document/d/1cMWaGzAnE0qYiKyfxXRL608ABgjOSogSoUtTZikSWYk/edit?usp=sharing\n\n` +
-        `**[OR-1/B] EĞİTİM KİTAPÇIĞI**\nhttps://docs.google.com/document/d/1FMD7mNXIrFa33H9INlOmr3ULbefwR63yV5BePwhGqgM/edit?usp=sharing\n\n` +
-        `**[OR-2] EĞİTİM KİTAPÇIĞI**\nhttps://docs.google.com/document/d/1MS-c8spE22DvTHccV2hsWoF99u_pPwsnogHO-IDUDvY/edit?usp=sharing\n\n` +
-        `**[OR-3 / OR-9] EĞİTİM KİTAPÇIĞI**\nhttps://docs.google.com/document/d/1ygwULEGoXN4xIioj9PAgK3K89ZSM7-Gkg73V7qfPsso/edit?usp=sharing`
+        `**[OR-1/A] EĞİTİM KİTAPÇIĞI**\n` +
+        `https://docs.google.com/document/d/1cMWaGzAnE0qYiKyfxXRL608ABgjOSogSoUtTZikSWYk/edit?usp=sharing\n\n` +
+        
+        `**[OR-1/B] EĞİTİM KİTAPÇIĞI**\n` +
+        `https://docs.google.com/document/d/1FMD7mNXIrFa33H9INlOmr3ULbefwR63yV5BePwhGqgM/edit?usp=sharing\n\n` +
+        
+        `**[OR-2] EĞİTİM KİTAPÇIĞI**\n` +
+        `https://docs.google.com/document/d/1MS-c8spE22DvTHccV2hsWoF99u_pPwsnogHO-IDUDvY/edit?usp=sharing\n\n` +
+        
+        `**[OR-3 / OR-9] EĞİTİM KİTAPÇIĞI**\n` +
+        `https://docs.google.com/document/d/1ygwULEGoXN4xIioj9PAgK3K89ZSM7-Gkg73V7qfPsso/edit?usp=sharing`
       )
-      .setFooter({ text: "Akademi işi, gönül işi" })
+      .setFooter({
+        text: "Akademi işi, Gönül İşi!",
+        iconURL: "https://i.ibb.co/v6mXmP0/akademi-logo.png"
+      })
       .setTimestamp();
 
     await interaction.reply({ embeds: [embed] });
