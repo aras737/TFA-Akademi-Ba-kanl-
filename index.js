078a7bc6eef8bfe1a1598aa5b6665bf17c55acfa
aras737 <erdemiraraseymen936@gmail.com>
2026-01-28 11:17:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on("interactionCreate", async (interaction) => {
     await interaction.deferReply().catch(() => null);
 
     const embed = new EmbedBuilder()
-      .setColor("#2b2d31")
+      .setColor("#006dff")
       .setAuthor({
         name: "Akademi Başkanlığı",
         iconURL: "https://i.ibb.co/L6vVv9N/akademi-logo.png" // Logonu buraya ekledim
@@ -40,7 +40,7 @@ client.on("interactionCreate", async (interaction) => {
         `**[OR-1/A] EĞİTİM KİTAPÇIĞI**\nhttps://docs.google.com/document/d/1cMWaGzAnE0qYiKyfxXRL608ABgjOSogSoUtTZikSWYk/edit?usp=sharing\n\n` +
         `**[OR-1/B] EĞİTİM KİTAPÇIĞI**\nhttps://docs.google.com/document/d/1FMD7mNXIrFa33H9INlOmr3ULbefwR63yV5BePwhGqgM/edit?usp=sharing\n\n` +
         `**[OR-2] EĞİTİM KİTAPÇIĞI**\nhttps://docs.google.com/document/d/1MS-c8spE22DvTHccV2hsWoF99u_pPwsnogHO-IDUDvY/edit?usp=sharing\n\n` +
-        `**[OR-3 / OR-9] EĞİTİM KİTAPÇIĞI**\nhttps://docs.google.com/document/d/1ygwULEGoXN4xIioj9PAgK3K89ZSM7-Gkg73V7qfPsso/edit?usp=sharing`
+        `**[OR-3 / OR-9] EĞİTİM KİTAPÇIĞI**\nhttps://docs.google.com/document/d/1ygwULEGoXN4xIioj9PAgK3K89ZSM7-Gkg73V7qfPsso/edit?usp=sharing\n\n`
       )
       .setFooter({
         text: "Akademi işi, Gönül İşi!",
